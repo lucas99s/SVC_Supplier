11c03252a90f6fe07e046dd64d7ea1b75ec04a10
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2024-03-09 17:35:56 -0300

    Páginas adicionadas

---
+++ SVC_Supplier/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SVC_Supplier.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        public IActionResult OrderDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Payment()
+        {
+            return View();
+        }
+    }
+}
